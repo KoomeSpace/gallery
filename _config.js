62fc89344f01805ba431a9e41f6ece818dab926e
KoomeSpace <koomebrian287@gmail.com>
2024-04-14 12:21:30 +0300

    Updated the _config.js to capture my mongodb details

--- _config.js
+++ _config.js
@@ -2,8 +2,12 @@ var config = {}
 
 // Update to have your correct username and password
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+    production: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/?retryWrites=true&w=majority&appName=bbkoome',
+    development: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/?retryWrites=true&w=majority&appName=bbkoome',
+    test: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/?retryWrites=true&w=majority&appName=bbkoome',
 }
+
+// Export the production MongoDB URI as an environment variable
+process.env.MONGODB_URI = config.mongoURI.production;
+
 module.exports = config;
