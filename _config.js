9615305fc62c25c1d14a8ab6a39b637142433b3f
KoomeSpace <160829320+KoomeSpace@users.noreply.github.com>
2024-04-15 09:48:06 +0000

    All configurations done

--- _config.js
+++ _config.js
@@ -2,9 +2,9 @@ var config = {};
 
 // Update to have your correct username, password, cluster name, and database name
 config.mongoURI = {
-    production: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/bbkoome?retryWrites=true&w=majority',
-    development: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/bbkoome?retryWrites=true&w=majority',
-    test: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/bbkoome?retryWrites=true&w=majority'
+    production: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/bbkoome?retryWrites=true&w=bbkoome',
+    development: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/bbkoome?retryWrites=true&w=bbkoome',
+    test: 'mongodb+srv://bbkoome:##Koome8322@bbkoome.eunvprz.mongodb.net/bbkoome?retryWrites=true&w=bbkoome'
 };
 
 // Export the production MongoDB URI as an environment variable
